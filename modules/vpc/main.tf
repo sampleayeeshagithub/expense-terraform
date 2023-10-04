545d8a32f3af96f4af9b4d7309cc6ad1486d542e
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-04 21:06:11 +0530

    Ansible

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -36,3 +36,12 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_eip" "ngw" {
   domain   = "vpc"
 }
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "${var.env}-ngw"
+  }
+}
