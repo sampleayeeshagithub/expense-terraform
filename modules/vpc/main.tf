d01cedd0032ae1273282d6573f40d2da6e415177
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-04 21:25:58 +0530

    Ansible

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,3 +45,29 @@ resource "aws_nat_gateway" "ngw" {
     Name = "${var.env}-ngw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = "private"
+  }
+}
