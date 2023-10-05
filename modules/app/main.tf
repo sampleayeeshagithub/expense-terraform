a4bdbc2d244d40c700c1d974382a2da955fde112
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-05 21:53:00 +0530

    Ansible

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -30,7 +30,47 @@ resource "aws_security_group" "security_group" {
     Name = "${var.env}-${var.component}-sg"
   }
 }
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.env}-${var.component}-policy"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:DescribeParameters",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters"
+    	  ],
+          "Resource": "*"
+        }
+      ]
+    })
+  }
+
+  tags = {
+    tag-key = "${var.env}-${var.component}-role"
+  }
+}
 
 resource "aws_launch_template" "template" {
   name = "${var.env}-${var.component}"
