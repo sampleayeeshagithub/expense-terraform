638c9d6544086a9ed5e34560689de5fc8dcfeb80
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-05 23:07:23 +0530

    Ansible

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -72,6 +72,11 @@ resource "aws_iam_role" "role" {
   }
 }
 
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
+}
+
 resource "aws_launch_template" "template" {
   name = "${var.env}-${var.component}"
   image_id = data.aws_ami.ami.id
@@ -79,7 +84,7 @@ resource "aws_launch_template" "template" {
   vpc_security_group_ids = [aws_security_group.security_group.id]
 
   iam_instance_profile {
-    name = aws_iam_role.role.name
+    name = aws_iam_instance_profile.instance_profile.name
   }
 
   user_data= base64encode(templatefile("${path.module}/userdata.sh", {
