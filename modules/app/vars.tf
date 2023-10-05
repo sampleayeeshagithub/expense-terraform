dcc7390f6ab9c1678effb53643cfb70026836d4a
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-05 16:16:40 +0530

    Ansible

--- modules/app/vars.tf
+++ modules/app/vars.tf
@@ -1,6 +1,6 @@
-variables "env" {}
-variables "component" {}
-variables "instance_type" {}
+variable "env" {}
+variable "component" {}
+variable "instance_type" {}
 variable "vpc_id" {}
 variable "app_port" {}
 variable "vpc_cidr" {}
