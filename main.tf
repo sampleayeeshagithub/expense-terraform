4e3b6fc4bb739d8ccdd8a8d301b33dd49d80e892
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-06 21:13:20 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -20,14 +20,15 @@ module "vpc" {
  # vpc_id            = module.vpc.vpc_id
 #}
 
-#module "private-lb" {
-#  source            = "./modules/alb"
-#  alb_type          = "private"
-#  env               = var.env
- # internal          = true
-#  subnets           = module.vpc.private_subnets
-#  vpc_id            = module.vpc.vpc_id
-#}
+module "private-lb" {
+  source            = "./modules/alb"
+  alb_sg_allow_cidr = var.vpc_cidr
+  alb_type          = "private"
+  env               = var.env
+  internal          = true
+  subnets           = module.vpc.private_subnets
+  vpc_id            = module.vpc.vpc_id
+}
 
 #module "frontend" {
 #  source           = "./modules/app"
