5b5ce855f87c4d0050b44ad43a7bfc5021ee0510
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-07 21:10:27 +0530

    Ansible

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "security_group" {
     cidr_blocks      = [var.alb_sg_allow_cidr]
   }
 
+ingress {
+    description      = "Https"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [var.alb_sg_allow_cidr]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -46,6 +54,20 @@ resource "aws_lb_listener" "listener-http" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = var.tg_arn
+  }
+}
+
+resource "aws_lb_listener" "listener-https" {
+  count             = var.alb_type == "public" ? 1 : 0
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:299627189740:certificate/7418ff7f-7659-4b2c-afed-2c825ddcaec8"
 
   default_action {
     type             = "forward"
